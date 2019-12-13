8dcb7705fadbbc74f746290dc435ab219db49aa2
Markus Tacker <Markus.Tacker@NordicSemi.no>
2019-12-13 15:24:26 +0100

    feat(uninstall): delete bifravst things
    See #83

--- cdk/createThingGroup.ts
+++ cdk/createThingGroup.ts
@@ -1,6 +1,7 @@
 import { Iot } from 'aws-sdk'
 import * as response from 'cfn-response'
 import { Context, CloudFormationCustomResourceEvent } from 'aws-lambda'
+import { paginate } from '../util/paginate'
 
 const iot = new Iot()
 
@@ -98,6 +99,64 @@ export const handler = async (
 				return p
 			})
 	} else {
+		await paginate({
+			paginator: async nextToken =>
+				iot
+					.listThingsInThingGroup({
+						thingGroupName: ThingGroupName,
+						nextToken,
+					})
+					.promise()
+					.then(async ({ things, nextToken }) => {
+						// Detach all the certificates, deactivate and delete them
+						// then delete the device
+						await Promise.all(
+							things?.map(async thing =>
+								iot
+									.listThingPrincipals({
+										thingName: thing,
+									})
+									.promise()
+									.then(async ({ principals }) =>
+										Promise.all(
+											principals?.map(async principal => {
+												const principalId = principal.split('/').pop() as string
+												return Promise.all([
+													iot
+														.detachThingPrincipal({
+															thingName: thing,
+															principal,
+														})
+														.promise(),
+													iot
+														.updateCertificate({
+															certificateId: principalId,
+															newStatus: 'INACTIVE',
+														})
+														.promise()
+														.then(async () =>
+															iot
+																.deleteCertificate({
+																	certificateId: principalId,
+																})
+																.promise(),
+														),
+												])
+											}) ?? [],
+										),
+									)
+									.then(async () =>
+										iot
+											.deleteThing({
+												thingName: thing,
+											})
+											.promise(),
+									),
+							) ?? [],
+						)
+						return nextToken
+					}),
+		})
 		console.log(`${RequestType} not supported.`)
 		response.send(
 			event,
