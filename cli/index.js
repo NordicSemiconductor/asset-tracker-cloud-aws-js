fa64e025b8b24f24278b0ad93e93d6751934b8a7
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-07-07 17:39:58 +0200

    feat: handle uncaught exceptions

--- cli/index.js
+++ cli/index.js
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
 
+process.on('uncaughtException', (err, origin) => {
+	process.exitCode = 1
+	console.error(`An unhandled exception occured!`)
+	console.error(`Exception origin: ${origin}`)
+	console.error(err)
+})
+
 // eslint-disable-next-line
 require('../dist/cli/bifravst')
