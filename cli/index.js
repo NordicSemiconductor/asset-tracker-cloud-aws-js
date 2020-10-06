3d839eb094b0c20d716db5cc33050857b4608a65
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-10-06 17:59:04 +0200

    fix(cli): die on errors

--- cli/index.js
+++ cli/index.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
-process.on('uncaughtException', (err, origin) => {
-	process.exitCode = 1
+const die = (err, origin) => {
 	console.error(`An unhandled exception occured!`)
-	console.error(`Exception origin: ${origin}`)
+	console.error(`Exception origin: ${JSON.stringify(origin)}`)
 	console.error(err)
-})
+	process.exit(1)
+}
+
+process.on('uncaughtException', die)
+process.on('unhandledRejection', die)
 
 // eslint-disable-next-line
 require('../dist/cli/bifravst')
