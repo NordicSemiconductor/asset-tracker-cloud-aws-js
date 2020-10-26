443f1a8fde8af485e8fe68913647d0bd4032a5db
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-10-26 12:40:15 +0100

    fix(ci): improve CDK context switch explanation

--- cdk/helper/enabledInContext.ts
+++ cdk/helper/enabledInContext.ts
@@ -20,20 +20,31 @@ export const enabledInContext = (node: CloudFormation.ConstructNode) => ({
 	onUndefined?: typeof ENABLED | typeof DISABLED
 }): boolean => {
 	const v = node.tryGetContext(key)
-	if (
-		v === (truthy ?? '1') ||
-		(v === undefined && (onUndefined ?? DISABLED) === ENABLED)
-	) {
-		console.error(
+	if (v === (truthy ?? '1') || (v === undefined && onUndefined === ENABLED)) {
+		const help = []
+		help.push(
 			chalk.gray(`Component`),
 			chalk.blueBright(component),
 			chalk.green('enabled.'),
-			chalk.gray(`Do not pass`),
-			chalk.grey.bold(`-c ${key}=${truthy ?? '1'}`),
-			chalk.gray(`to`),
-			chalk.grey.bold(`npx cdk deploy '*'`),
-			chalk.grey(`to disable.`),
 		)
+		if (v === undefined && onUndefined === ENABLED) {
+			help.push(
+				chalk.gray(`Pass`),
+				chalk.grey.bold(`-c ${key}=0`),
+				chalk.gray(`to`),
+				chalk.grey.bold(`npx cdk deploy '*'`),
+				chalk.grey(`to disable.`),
+			)
+		} else {
+			help.push(
+				chalk.gray(`Do not pass`),
+				chalk.grey.bold(`-c ${key}=${truthy ?? '1'}`),
+				chalk.gray(`to`),
+				chalk.grey.bold(`npx cdk deploy '*'`),
+				chalk.grey(`to disable.`),
+			)
+		}
+		console.error(...help)
 		onEnabled?.()
 		return true
 	}
@@ -42,7 +53,7 @@ export const enabledInContext = (node: CloudFormation.ConstructNode) => ({
 		chalk.grey.bold(component),
 		chalk.gray('disabled.'),
 	]
-	if ((onUndefined ?? DISABLED) === ENABLED) {
+	if (onUndefined === ENABLED) {
 		help.push(
 			chalk.gray(`Do not pass`),
 			chalk.grey.bold(`-c ${key}=${v}`),
