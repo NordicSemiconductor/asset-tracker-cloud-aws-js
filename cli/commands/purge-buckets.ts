193d967fde329ca85bdb1aae05f8e4cf1ae3a674
Markus Tacker <m@coderbyheart.com>
2020-12-02 11:46:05 +0100

    fix(cli): improve purge-buckets
    This now uses the stack resources to find buckets
    and paginates bucket contents

--- cli/commands/purge-buckets.ts
+++ cli/commands/purge-buckets.ts
@@ -1,37 +1,44 @@
 import { CommandDefinition } from './CommandDefinition'
-import { stackOutput } from '@bifravst/cloudformation-helpers'
 import { S3, CloudFormation } from 'aws-sdk'
 import * as chalk from 'chalk'
 import { retry } from './retry'
 import { region } from '../../cdk/regions'
-import { CORE_STACK_NAME } from '../../cdk/stacks/stackName'
+import {
+	CORE_STACK_NAME,
+	DEVICEUI_STACK_NAME,
+	WEBAPP_STACK_NAME,
+	CONTINUOUS_DEPLOYMENT_STACK_NAME,
+	FIRMWARE_CI_STACK_NAME,
+} from '../../cdk/stacks/stackName'
+import { paginate } from '../../util/paginate'
+
+const cf = new CloudFormation({ region })
+
+const listBuckets = async (StackName: string) =>
+	cf
+		.describeStackResources({ StackName })
+		.promise()
+		.then(
+			(res) =>
+				res?.StackResources?.filter(
+					({ ResourceType }) => ResourceType === 'AWS::S3::Bucket',
+				).map(({ PhysicalResourceId }) => PhysicalResourceId as string) ?? [],
+		)
+		.catch(({ message }) => {
+			console.warn(chalk.yellow.dim(message))
+			return []
+		})
 
 export const purgeBucketsCommand = (): CommandDefinition => ({
 	command: 'purge-buckets',
 	action: async () => {
-		const {
-			historicalDataQueryResultsBucketName,
-			avatarBucketName,
-			historicalDataBucketName,
-			webAppBucketName,
-			deviceUiBucketName,
-			fotaBucketName,
-		} = {
-			...(await stackOutput(new CloudFormation({ region }))(CORE_STACK_NAME)),
-		} as { [key: string]: string }
 		const buckets = [
-			historicalDataQueryResultsBucketName,
-			avatarBucketName,
-			historicalDataBucketName,
-			webAppBucketName,
-			deviceUiBucketName,
-			fotaBucketName,
+			...(await listBuckets(CORE_STACK_NAME)),
+			...(await listBuckets(WEBAPP_STACK_NAME)),
+			...(await listBuckets(DEVICEUI_STACK_NAME)),
+			...(await listBuckets(CONTINUOUS_DEPLOYMENT_STACK_NAME)),
+			...(await listBuckets(FIRMWARE_CI_STACK_NAME)),
 		]
-		console.log('Purging bucket:')
-		buckets
-			.filter((b) => b)
-			.forEach((b) => console.log(chalk.grey('-'), chalk.yellow(b)))
-
 		const s3 = new S3({ region })
 		await Promise.all(
 			buckets
@@ -43,30 +50,37 @@ export const purgeBucketsCommand = (): CommandDefinition => ({
 							3,
 							() => 5000,
 						)(async () => {
-							const { Contents } = await s3
-								.listObjects({ Bucket: bucketName })
-								.promise()
-							if (!Contents) {
-								console.log(`${bucketName} is empty.`)
-							} else {
-								await Promise.all(
-									Contents.map(async (obj) => {
-										console.log(bucketName, obj.Key)
-										return s3
-											.deleteObject({
-												Bucket: bucketName,
-												Key: `${obj.Key}`,
-											})
-											.promise()
-									}),
-								)
-							}
+							await paginate({
+								paginator: async (nextMarker?: string) => {
+									const { Contents, Marker } = await s3
+										.listObjects({ Bucket: bucketName, Marker: nextMarker })
+										.promise()
+									if (!Contents) {
+										console.log(`${bucketName} is empty.`)
+									} else {
+										await Promise.all(
+											Contents.map(async (obj) => {
+												console.log(bucketName, obj.Key)
+												return s3
+													.deleteObject({
+														Bucket: bucketName,
+														Key: `${obj.Key}`,
+													})
+													.promise()
+											}),
+										)
+									}
+									return Marker
+								},
+							})
 							await s3.deleteBucket({ Bucket: bucketName }).promise()
 							console.log(`${bucketName} deleted.`)
 						})
 					} catch (err) {
 						console.error(
-							`Failed to purge bucket ${bucketName}: ${err.message}`,
+							chalk.yellow.dim(
+								`Failed to purge bucket ${bucketName}: ${err.message}`,
+							),
 						)
 					}
 				}),
