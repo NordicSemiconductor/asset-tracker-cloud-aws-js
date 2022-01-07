f87eb1e5f92f5009f60df2dd305836eb1090080e
Markus Tacker <Markus.Tacker@NordicSemi.no>
2022-01-07 12:17:39 +0100

    fix(cli): convert to use ESM

--- cli/index.js
+++ cli/index.js
@@ -1,14 +1,12 @@
-#!/usr/bin/env node
-
 const die = (err, origin) => {
 	console.error(`An unhandled exception occured!`)
 	console.error(`Exception origin: ${JSON.stringify(origin)}`)
 	console.error(err)
-	process.exit(1)
+	if (process.env.DONT_DIE_ON_UNHANDLED_EXCEPTIONS === undefined)
+		process.exit(1)
 }
 
 process.on('uncaughtException', die)
 process.on('unhandledRejection', die)
 
-// eslint-disable-next-line
-require('../dist/cli/cli')
+import('../dist/cli/cli.js')
