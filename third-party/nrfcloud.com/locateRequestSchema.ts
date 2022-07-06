92d288154aa7f0607120863bde7188a799b4d4ab
Markus Tacker <Markus.Tacker@NordicSemi.no>
2022-07-06 16:20:16 +0200

    fix: unions in Typebox now work differently

---
+++ third-party/nrfcloud.com/locateRequestSchema.ts
@@ -0,0 +1,23 @@
+import { Type } from '@sinclair/typebox'
+
+const cells = Type.Array(
+	Type.Object(
+		{
+			eci: Type.Integer({ minimum: 1 }),
+			mcc: Type.Integer({ minimum: 100, maximum: 999 }),
+			mnc: Type.Integer({ minimum: 1, maximum: 99 }),
+			tac: Type.Integer({ minimum: 1 }),
+		},
+		{ additionalProperties: false },
+	),
+	{ minItems: 1 },
+)
+
+export const locateRequestSchema = Type.Union([
+	Type.Object({
+		lte: cells,
+	}),
+	Type.Object({
+		nbiot: cells,
+	}),
+])
