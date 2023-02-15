0cf6782ca808019d1e01216733e2081f942d1aab
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-02-15 17:15:58 +0100

    fix(cd): upgrade to aws/codebuild/standard:6.0

--- cdk/stacks/ContinuousDeployment.ts
+++ cdk/stacks/ContinuousDeployment.ts
@@ -122,7 +122,7 @@ export class ContinuousDeploymentStack extends CloudFormation.Stack {
 			environment: {
 				type: 'LINUX_CONTAINER',
 				computeType: 'BUILD_GENERAL1_LARGE',
-				image: 'aws/codebuild/standard:5.0',
+				image: 'aws/codebuild/standard:6.0',
 				environmentVariables: [
 					{
 						name: 'GH_TOKEN',
