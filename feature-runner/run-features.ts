6323ffb9834d78d468fba9608cd327b50e41cd52
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-09-13 00:01:07 +0200

    fix: naive migration of API
    This will certainly not pass the tests,
    because retry logic is missing.

--- feature-runner/run-features.ts
+++ feature-runner/run-features.ts
@@ -95,29 +95,29 @@ const runner = await runFolder<World & Record<string, any>>({
 		onDebug: (info, ...args) =>
 			console.error(
 				ts(),
-				chalk.magenta.dim(info.context.keyword),
-				chalk.magenta(info.context.title),
+				chalk.magenta.dim(info.step.keyword),
+				chalk.magenta(info.step.title),
 				...args.map((arg) => chalk.cyan(print(arg))),
 			),
 		onError: (info, ...args) =>
 			console.error(
 				ts(),
-				chalk.magenta.dim(info.context.keyword),
-				chalk.magenta(info.context.title),
+				chalk.magenta.dim(info.step.keyword),
+				chalk.magenta(info.step.title),
 				...args.map((arg) => chalk.red(print(arg))),
 			),
 		onInfo: (info, ...args) =>
 			console.error(
 				ts(),
-				chalk.magenta.dim(info.context.keyword),
-				chalk.magenta(info.context.title),
+				chalk.magenta.dim(info.step.keyword),
+				chalk.magenta(info.step.title),
 				...args.map((arg) => chalk.green(print(arg))),
 			),
 		onProgress: (info, ...args) =>
 			console.error(
 				ts(),
-				chalk.magenta.dim(info.context.keyword),
-				chalk.magenta(info.context.title),
+				chalk.magenta.dim(info.step.keyword),
+				chalk.magenta(info.step.title),
 				...args.map((arg) => chalk.yellow(print(arg))),
 			),
 	},
