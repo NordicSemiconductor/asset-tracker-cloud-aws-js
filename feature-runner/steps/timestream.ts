13e2df75325a19d764d4b47a7c3232ad217dfb2a
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-09-14 14:42:25 +0200

    test(e2e): implement retry for timestream

--- feature-runner/steps/timestream.ts
+++ feature-runner/steps/timestream.ts
@@ -5,48 +5,72 @@ import {
 import {
 	codeBlockOrThrow,
 	type StepRunner,
-	type StepRunnerArgs,
 } from '@nordicsemiconductor/bdd-markdown'
 import { parseResult } from '@nordicsemiconductor/timestream-helpers'
 import type { World } from '../run-features.js'
 import type { UserCredentials } from './cognito.js'
+import { retryCheck } from './retryCheck.js'
+import { arrayMatching, check, objectMatching } from 'tsmatchers'
 
 type TimestreamWorld = World & {
-	timestreamQueryResult?: Record<string, any>[]
 	cognito?: UserCredentials
 }
-const steps: StepRunner<TimestreamWorld>[] = [
-	{
-		match: (title) => /^I run this Timestream query$/.test(title),
-		run: async ({
-			context,
-			step,
-			log: { progress },
-		}: StepRunnerArgs<TimestreamWorld>): Promise<void> => {
-			if (context.cognito === undefined)
-				throw new Error(`Cognito authentication not available.`)
 
-			const { code: query } = codeBlockOrThrow(step)
-
-			const timestream = new TimestreamQueryClient({
-				credentials: {
-					secretAccessKey: context.cognito.SecretKey,
-					accessKeyId: context.cognito.AccessKeyId,
-					sessionToken: context.cognito.SessionToken,
-				},
-			})
-
-			progress('timestream', query)
-			const res = await timestream.send(
+const retryableQuery = (client: TimestreamQueryClient, QueryString: string) => {
+	let result: Record<string, any>[] = []
+	return {
+		send: async () => {
+			const res = await client.send(
 				new QueryCommand({
-					QueryString: query,
+					QueryString,
 				}),
 			)
-
-			const data = parseResult(res)
-			context['timestreamQueryResult'] = data
+			result = parseResult(res)
 		},
-	},
-]
+		result: () => result,
+	}
+}
+
+const steps = (): StepRunner<TimestreamWorld>[] => {
+	let currentQuery: ReturnType<typeof retryableQuery> | undefined = undefined
+
+	return [
+		{
+			match: (title) => /^I run this Timestream query$/.test(title),
+			run: async ({ context, step, log: { progress } }): Promise<void> => {
+				const { code: query } = codeBlockOrThrow(step)
+
+				if (context.cognito === undefined)
+					throw new Error(`Cognito authentication not available.`)
+				const timestream = new TimestreamQueryClient({
+					credentials: {
+						secretAccessKey: context.cognito.SecretKey,
+						accessKeyId: context.cognito.AccessKeyId,
+						sessionToken: context.cognito.SessionToken,
+					},
+				})
+
+				progress('timestream', query)
+				currentQuery = retryableQuery(timestream, query)
+				await currentQuery.send()
+			},
+		},
+		{
+			match: (title) => /^the Timestream result should match$/.test(title),
+			run: async ({ step }) =>
+				retryCheck(
+					() => {
+						const code = codeBlockOrThrow(step)
+						const expected: Record<string, any>[] = JSON.parse(code.code)
+
+						check(currentQuery?.result() ?? []).is(
+							arrayMatching(expected.map((o) => objectMatching(o))),
+						)
+					},
+					async () => {},
+				),
+		},
+	]
+}
 
 export default steps
